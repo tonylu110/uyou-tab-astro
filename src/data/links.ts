b44f8704558afdbac2f1c60c09457bd469a2e329
Anthony Lu <58923223+tonylu110@users.noreply.github.com>
2023-06-11 15:43:03 +0800

    perf: change to data

---
+++ src/data/links.ts
@@ -0,0 +1,40 @@
+import edgeImg from '../assets/images/Edge.png'
+import chromeImg from '../assets/images/chrome.png'
+import githubImg from '../assets/images/GitHub.png'
+import crxImg from '../assets/images/crx.png'
+import extImg from '../assets/images/extension.png'
+
+const list = [
+  {
+    title: 'edge 扩展商店',
+    icon: edgeImg,
+    url: 'https://microsoftedge.microsoft.com/addons/detail/uyou-%E6%96%B0%E6%A0%87%E7%AD%BE%E9%A1%B5/jlgjnfcoogajiefbjcmokohhpoedpdhh'
+  },
+  {
+    title: 'chrome 网上应用店',
+    icon: chromeImg,
+    url: 'https://chrome.google.com/webstore/detail/uyou-%E6%96%B0%E6%A0%87%E7%AD%BE%E9%A1%B5/ecdbpdmfhdnklemcfooinlnjilnlgdnl?hl=zh-CN&authuser=0'
+  },
+  {
+    title: 'GitHub 主页',
+    icon: githubImg,
+    url: 'https://github.com/tonylu110/uyouNewTab'
+  },
+  {
+    title: '离线版下载 密码:9ye0',
+    icon: crxImg,
+    url: 'https://tonylu.lanzoup.com/b0efdn2tc'
+  },
+  {
+    title: 'crx 下载 密码:9ixn',
+    icon: crxImg,
+    url: 'https://tonylu.lanzouf.com/b0efks32d'
+  },
+  {
+    title: '网页版 beta',
+    icon: extImg,
+    url: 'https://uyoutab.uyou.org.cn'
+  }
+]
+
+export default list
